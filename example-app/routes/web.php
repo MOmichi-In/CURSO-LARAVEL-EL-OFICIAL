c9a2570adba80b786577a3f53d505275d63e3afb
Al-i <aliandresmejia77@gmail.com>
2025-02-17 13:42:16 -0500

    Eloquent
    eloquent es un framework aparte de laravel que se define como ORM (object-relacional-mapper), el cual nos permite interactuar con bases de datos de manera sencilla, sin la necesidad de implementar sentencias directas para manipular la base de datos, en este caso se esta implementando mysql para este proyecto

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,10 +1,30 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use function Laravel\Prompts\select;
 
 Route::get('/', [HomeController::class, '_summon']);
 Route::get('/post', [PostController::class, 'index']);
 Route::get('/post/post-0', [PostController::class, 'create']);
 Route::get('/post/{post}', [PostController::class, 'show']);
+Route::get('prueba', function(){
+    // $post = new Post;
+    // $post->title = 'Titulo de prueba 2';
+    // $post->content = 'Contenido de prueba 2';
+    // $post->categoria = 'Categoria de prueba 2';
+
+    // añadir contenido a la base de datos sin tener que hacer sentencias MYSQL
+
+    // $post = Post::orderBy('id', 'asc')
+    // ->select('id', 'title', 'categoria')
+    // ->take(2)
+    // ->get();
+
+    // $post = Post::find(1);
+    // $post->delete();
+
+    // return $post;
+});
