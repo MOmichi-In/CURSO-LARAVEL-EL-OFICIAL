6dac632432864e8d750d2abb9815f8643027e147
Al-i <aliandresmejia77@gmail.com>
2025-02-20 10:48:21 -0500

    Route resourse
    existe un metodo llamado resource en el cual solo con pasarle el controlador y la clase hace que laravel interprete automaticamente las rutas segun los metodos implementados en la clase

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -7,13 +7,14 @@ use App\Http\Controllers\PostController;
 use function Laravel\Prompts\select;
 
 Route::get('/', [HomeController::class, '_summon'])->name('home');
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
-Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
-Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+Route::resource('posts', PostController::class);
+// Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+// Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+// Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+// Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+// Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+// Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+// Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 // Route::get('prueba', function(){
     // $post = new Post;
     // $post->title = 'titulo de prueba 3';
